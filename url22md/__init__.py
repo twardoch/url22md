bf94ae0ad6ff6bd7775907511d7668a5be6290fa
Adam Twardoch <adam@twardoch.com>
2026-03-26 23:24:20 +0100

    v1.0.2

--- url22md/__init__.py
+++ url22md/__init__.py
@@ -1,9 +1,16 @@
 """url22md - Convert HTTP(S) URLs to Markdown."""
 
+from __future__ import annotations
+
 # this_file: url22md/__init__.py
 this_file = "url22md/__init__.py"
 
-__version__ = "1.0.0"
+from importlib.metadata import version
+
+try:
+    __version__ = version("url22md")
+except Exception:
+    __version__ = "0.0.0"
 
 from url22md.converter import run_conversion
 from url22md.tools import QUALITY_THRESHOLD, TOOLS, ToolResult, assess_quality
